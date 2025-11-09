b25cf17d917b8fab4602af9508a0f82063912a01
Danilson Sanches (upskill217) <upskill217@alunos.fc.ul.pt>
2025-11-09 20:31:56 +0000

    feat: criando chat Controller

---
+++ data/chatController.js
@@ -0,0 +1,12 @@
+export const chatController = async (req, res) => {
+
+  const { message } = req.body;
+
+  if (!message) {
+    return res.status(400).json({ message: "É obrigatório preencher a mensagem!" });
+  }
+
+  const answer = await handleHealthQuery(message);
+
+  return res.status(200).json({reply: answer});
+};
